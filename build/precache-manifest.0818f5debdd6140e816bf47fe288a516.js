383a66885815432138f170fcd7e20f3c174a3ffd
Bruno Targhetta <bruno_targhetta@hotmail.com>
2019-05-12 22:38:21 -0300

    Initial Commit

---
+++ build/precache-manifest.0818f5debdd6140e816bf47fe288a516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ca614bbaffcb939378ab4a8232121f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa27f75eb6cac981c103",
+    "url": "/static/css/main.3f27033c.chunk.css"
+  },
+  {
+    "revision": "453ded4a47a1a1a2e733",
+    "url": "/static/js/2.774c5952.chunk.js"
+  },
+  {
+    "revision": "fa27f75eb6cac981c103",
+    "url": "/static/js/main.11a4783e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
